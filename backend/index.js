76a53c3a978b901fabd3b0c9917331f0f6fcc202
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2023-12-28 15:15:38 +0530

    add backend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { dbConnection } = require("./configs/db");
+const { userRouter } = require("./routes/userRouter");
+
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    res.send(`<h1>Home page</h1>`)
+})
+
+app.use("/api/auth", userRouter);
+
+app.listen(port, async () => {
+  try {
+    await dbConnection;
+    console.log(`server is connected DB`);
+  } catch (error) {
+    console.log("error while connecting to DB");
+  }
+  console.log(`server is running on ${port}`);
+});
