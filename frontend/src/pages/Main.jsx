f916db23829a583fe7f27cf659ec27f70f6f4aed
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2023-12-29 12:03:20 +0530

    logout

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,11 +1,21 @@
+import { Button } from '@chakra-ui/react'
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Main = () => {
+  const navigate=useNavigate()
+  const handleLogout = () => {
+   
+    localStorage.removeItem('token');
+   navigate("/")
+  };
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
        <h1>
        Welcome to The Brand Wick Home Page
         </h1> 
+        <Button colorScheme="red" 
+        mt={4} onClick={handleLogout}>Log Out</Button>
         </div>
   )
 }
